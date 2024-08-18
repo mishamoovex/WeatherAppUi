0e9b594feb54e7f50930ef6929e3c04d174ce36c
mykhailo.development@gmail.com <vasylemv@fuib.com>
2024-08-18 11:42:37 +0300

    Dependencies set up

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,6 @@ android {
         versionCode = 1
         versionName = "1.0"
 
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
             useSupportLibrary = true
         }
@@ -50,20 +49,10 @@ android {
 }
 
 dependencies {
-
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.androidx.activity.compose)
-    implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.ui)
-    implementation(libs.androidx.ui.graphics)
-    implementation(libs.androidx.ui.tooling.preview)
-    implementation(libs.androidx.material3)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.ui.test.junit4)
-    debugImplementation(libs.androidx.ui.tooling)
-    debugImplementation(libs.androidx.ui.test.manifest)
+    //AndroidX
+    implementation(libs.bundles.androidX)
+    //Compose
+    implementation(platform(libs.compose.bom))
+    debugImplementation(libs.compose.tooling)
+    implementation(libs.bundles.ui)
 }
